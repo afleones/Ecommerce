43faf7384aad8be59cdc24f61bcf105539ea705e
Afleones <afleones@misena.edu.co>
2021-03-19 13:35:53 -0500

    Creacion del Proyecto, instalacion de plantilla Ecommerce, lado del cliente

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,15 @@
+import Rails from "@rails/ujs"
+import "@hotwired/turbo-rails"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap"
+
+Rails.start()
+ActiveStorage.start()
+
+
+
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
